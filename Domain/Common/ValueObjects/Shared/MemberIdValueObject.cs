7a7bfa9749508a7785d3a08ca152486371405f4b
Loo Linn Ljungstedt <Loo.Ljungs@gmail.com>
2026-03-13 22:22:47 +0100

    Refactor ValueObjects to use shared base and DomainException

---
+++ Domain/Common/ValueObjects/Shared/MemberIdValueObject.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common.ValueObjects.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Common.ValueObjects.Shared;
+
+public record MemberId(Guid Value) : GuidValueObject(Value, DomainErrors.Validation.Required);
---
+++ Domain/Sessions/ValueObjects/SessionValueObjects.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Common;
+using Domain.Common.Exceptions;
+using Domain.Common.ValueObjects.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Sessions.ValueObjects;
+
+public record Title(string Value) : StringValueObject(Value, 3, 20);
+public record Instructor(string Value) : StringValueObject(Value, 3, 100);
+public record Description(string Value) : StringValueObject(Value, 0, 500);
+public record Capacity(int Value) : RangeValueObject(Value, 10, 40, DomainErrors.Session.InvalidCapacity);
+public record TimeSlot
+{
+    public DateTime StartTime { get; }
+    public DateTime EndTime { get; }
+
+    public TimeSlot(DateTime start, DateTime end)
+    {
+        if (end <= start)
+            throw new DomainException(DomainErrors.Session.InvalidDate);
+
+        if (start < DateTime.UtcNow)
+            throw new DomainException(DomainErrors.Session.InvalidDate);
+
+        StartTime = start;
+        EndTime = end;
+    }
+    public TimeSpan Duration => EndTime - StartTime;
+}
