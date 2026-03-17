407bb68f995687312316002c17a13dceeab5e557
Loo Linn Ljungstedt <Loo.Ljungs@gmail.com>
2026-03-17 15:24:36 +0100

    implement applicationlayer for forms

---
+++ Application/CustomerSupport/IContactFormService.cs
@@ -0,0 +1,8 @@
+﻿using Application.CustomerSupport.Input;
+
+namespace Application.CustomerSupport;
+
+public interface IContactFormService
+{
+    Task RegisterContactRequestAsync(RegisterContactRequestInput request, CancellationToken ct = default);
+}
