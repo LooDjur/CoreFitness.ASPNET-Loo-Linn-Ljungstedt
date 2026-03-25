04ceba18a6dbb4c10dad76aefd6d6dc0cfd5edba
Loo Linn Ljungstedt <Loo.Ljungs@gmail.com>
2026-03-25 11:32:49 +0100

    layout

---
+++ Presentation.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Controllers.Common;
+
+namespace Presentation.WebApp.Controllers;
+
+public class AccountController : BaseController
+{
+    [HttpGet]
+    [Route("account/about-me")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
